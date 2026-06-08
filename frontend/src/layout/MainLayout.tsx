a33fb2fb190457d82c364e98dff8c00fcf68b786
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-08 17:58:56 +0300

    Update MainLayout.tsx

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,25 +1,23 @@
-import { ReactNode } from "react";
-import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
 
-const MainLayout = ({ children }: { children: ReactNode }) => {
+export default function MainLayout() {
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
+    <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
       <Sidebar />
 
-      {/* Main Content Area */}
-      <div className="flex-1 flex flex-col overflow-hidden">
-        {/* Top Navigation */}
+      {/* Main Area */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        {/* Top Navbar */}
         <Navbar />
 
         {/* Page Content */}
-        <main className="flex-1 overflow-auto p-8 bg-gray-50">
-          {children}
+        <main className="flex-1 overflow-y-auto p-6">
+          <Outlet />
         </main>
       </div>
     </div>
   );
-};
-
-export default MainLayout;
+}
