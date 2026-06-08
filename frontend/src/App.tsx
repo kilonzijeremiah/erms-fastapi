0145b603373de6264467779e5f4a1d2b301098cd
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-08 13:05:17 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Students from './pages/Students';
-import Login from './pages/Login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/*" element={<Sidebar />}>
           <Route index element={<Dashboard />} />
           <Route path="students" element={<Students />} />
