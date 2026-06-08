d7108e29dae9549b49be031ee5ba2579207aa106
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-08 15:29:50 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import Students from './pages/Students';
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Sidebar />}>
-          <Route index element={<Dashboard />} />
-          <Route path="students" element={<Students />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
-};
+function App() {
+return ( <BrowserRouter> <Routes>
+<Route path="/" element={<Sidebar />}>
+<Route index element={<Dashboard />} />
+<Route path="students" element={<Students />} /> </Route> </Routes> </BrowserRouter>
+);
+}
 
 export default App;
