b788698a50bcc3734cc091cdd1fbbb0a76b645a7
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-08 16:56:37 +0300

    Refactor App component for improved readability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages/Dashboard';
-import Students from './pages/Students';
+import Dashboard from "./pages/Dashboard";
+import Students from "./pages/Students";
 
-function App() {
-return ( <BrowserRouter> <Routes>
-<Route path="/" element={<Sidebar />}>
-<Route index element={<Dashboard />} />
-<Route path="students" element={<Students />} /> </Route> </Routes> </BrowserRouter>
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Sidebar />}>
+          <Route index element={<Dashboard />} />
+          <Route path="students" element={<Students />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
