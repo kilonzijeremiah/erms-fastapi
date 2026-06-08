5a72af9ff99ebdc53bb6eda432c783e243c5dd8b
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-08 14:07:56 +0300

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Dashboard = () => {
   return (
     <div>
-      <h1 className="text-3xl font-semibold">Dashboard</h1>
-      <p>Welcome to Ikonex Academy</p>
+      <h1 className="text-3xl font-semibold text-gray-900">Dashboard</h1>
+      <p className="mt-4 text-gray-600">Welcome to Ikonex Academy Student Management System</p>
     </div>
   );
 };
