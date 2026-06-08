6a8d9d5026b4fb71756396f1a7418ee6fc91913f
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-08 17:27:35 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-
-import Dashboard from "./pages/Dashboard";
-import Students from "./pages/Students";
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Sidebar />}>
-          <Route index element={<Dashboard />} />
-          <Route path="students" element={<Students />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>APP WORKS</h1>;
 }
