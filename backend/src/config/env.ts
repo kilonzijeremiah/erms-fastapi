0b7744a3dc0890366fc5b401c196501957da06ad
Kilonzi Jeremiah <kilonzijeremiah009@gmail.com>
2026-06-12 13:16:39 +0300

    Delete backend directory

--- backend/src/config/env.ts
+++
@@ -1,4 +0,0 @@
-export const ENV = {
-  JWT_SECRET: "super_secure_secret_change_me",
-  PORT: 8000
-};
--- backend/src/routes/dashboard.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getDashboardStats } from "../controllers/dashboard.controller";
-
-const router = Router();
-
-router.get("/", getDashboardStats);
-
-export default router;
